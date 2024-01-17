0a97709986f92ac119b82434f77aec611c1bafc4
Abraham <abrahamnhier97@gmail.com>
2024-01-17 10:03:09 +0300

    set up routes

---
+++ Controllers/tasks.js
@@ -0,0 +1,9 @@
+
+
+const getAllTasks = (req,res) => {
+res.send('all items on file')
+};
+
+module.exports = {
+    getAllTasks,
+}
--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const tasks = require('./Routes/Tasks')
 
+// middleware
+app.use(express.json());
+
+
+// routes
 app.get('/hello', (req,res) => {
     res.send('Homepage')
 });
+app.use('/api/v1/tasks',tasks);
 
 const port = 3000;
 app.listen(port,console.log(`server is listening on port ${port}...`)
