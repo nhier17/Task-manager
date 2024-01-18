3e33e29fe1157e0e6fa533192f0de0e869ef8ca8
Abraham <abrahamnhier97@gmail.com>
2024-01-18 08:52:54 +0300

    set up connection

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-require('./db/connect')
+
 const express = require('express');
 const app = express();
 const tasks = require('./Routes/Tasks')
-
+const connectDB = require('./db/connect')
+require('dotenv').config()
 // middleware
 app.use(express.json());
 
@@ -14,5 +15,14 @@ app.get('/hello', (req,res) => {
 app.use('/api/v1/tasks',tasks);
 
 const port = 3000;
+const start = async () => {
+    try {
+await connectDB(process.env.MONGO_URI);
 app.listen(port,console.log(`server is listening on port ${port}...`)
 );
+    } catch (error) {
+console.log(error)
+    }
+};
+start();
+
--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
 
-const connectionString ='mongodb+srv://Abraham:password17@nodeexpressprojects.yf8ck2r.mongodb.net/'
-mongoose
-.connect(connectionString)
-.then(() => console.log('connected to the db...'))
-.catch((err) =>console.log(err))
+
+
+const connectDB = (url) => {
+    return mongoose
+    .connect(url)
+    .then(() => console.log('connected to the db...'))
+    .catch((err) =>console.log(err))
+}
+module.exports = connectDB;
+
